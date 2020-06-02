1589bb7fa401ad9dd4a4cb2fb87247ee37eaf8f4
randyyu13 <ldyu999@gmail.com>
2020-06-01 23:43:07 -0500

    home page update
    more cool animation shit

--- chess/src/components/homeBody.jsx
+++ chess/src/components/homeBody.jsx
@@ -3,25 +3,11 @@ import chessboard from "./pictures/3DChessboard.gif";
 
 class HomeBody extends Component {
   state = {};
-  styles = {
-    marginTop: 0,
-    textAlign: "center",
-    padding: 100,
-    fontSize: 72,
-    fontFamily: "Times New Roman",
-    marginLeft: 570,
-  };
   render() {
     return (
-      <div>
-        <img
-          style={this.styles}
-          src={chessboard}
-          alt=""
-          height="500"
-          width="500"
-        />
-      </div>
+      <a href="https://www.google.com/">
+        <img className="undraggable chessboard" src={chessboard} alt="" />
+      </a>
     );
   }
 }
---
+++ chess/src/components/instructions.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class instructions extends Component {
+  state = {};
+  render() {
+    return;
+  }
+}
+
+export default instructions;
+//supposed to be sidebar to explain the rules
