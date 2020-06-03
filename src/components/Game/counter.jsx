66657865083c41e95ec78078c40e223f05a7732e
ssun914 <stevensun914@utexas.edu>
2020-06-03 18:53:28 -0500

    testing load counter

--- src/components/Game/counter.jsx
+++ src/components/Game/counter.jsx
@@ -7,19 +7,31 @@ class Counter extends Component {
 
     this.database = firebase.database();
     this.counter = this.database.ref().child('TEST_COUNTER');
+    this.loads = this.database.ref().child('LOADS');
 
     this.state = {
-      count: 0
+      count: 0,
+      loads: 0
     }
   }
 
   componentDidMount() {
+    this.loads.on('value', snap => {
+      this.setState({
+        loads: snap.val()
+      })
+    })
+    this.loads.set(this.state.loads + 1);
     this.counter.on('value', snap => {
       this.setState({
         count: snap.val()
       })
     })
   }
+
+  handleLoad() {
+    this.loads.set(this.state.loads + 1);
+   }
 
   incrementCounter = () => {
       this.counter.set(this.state.count + 1);
@@ -29,6 +41,7 @@ class Counter extends Component {
     return (
         <React.Fragment>
             <h1 className="head">This counter has been incremented: {this.state.count} times</h1>
+            <div>This page has been loaded: {this.state.loads} times</div>
             <button type="button" onClick={this.incrementCounter} class="btn btn-primary">Increment</button>
         </React.Fragment>
     );
