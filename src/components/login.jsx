42ee420287ebffd12099b8ad35ebb55e3bfd57ee
randyyu13 <ldyu999@gmail.com>
2020-06-05 03:26:06 -0500

    implemented login (purely css and html, still need a signup and implement authorization)

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -3,7 +3,29 @@ import React, { Component } from "react";
 class Login extends Component {
   state = {};
   render() {
-    return <p>sign in</p>;
+    return (
+      <form className="testForm">
+        <div className="container">
+          <label className> Username </label>
+          <input
+            type="text"
+            placeholder="Enter Username"
+            name="username"
+            required
+          />
+          <br />
+          <label> Password</label>
+          <input
+            type="password"
+            placeholder="Enter Password"
+            name="password"
+            required
+          />
+        </div>
+
+        <button type="submit">Login</button>
+      </form>
+    );
   }
 }
 
