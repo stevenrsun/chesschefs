a4d6fd0b2cf52723577a8b55404cebf848df5622
randyyu13 <ldyu999@gmail.com>
2020-06-10 01:07:57 -0500

    update
    .

--- src/components/Game/chessboard.jsx
+++ src/components/Game/chessboard.jsx
@@ -1,58 +1,106 @@
-import React, { Component } from 'react';
-import Square from './square';
+import React, { Component } from "react";
+import Square from "./square";
 
 class Chessboard extends Component {
-    state = {  }
-    styles={
-        board: {
-            width: '512px',
-            height: '512px'
-        },
-        square: {
-            width: '64px',
-            height: '64px'
-        }
-    }
-    render() { 
-        return ( 
-            <div class="container ml-4">
-                <div class="row" style={this.styles.board}>
-                    <div class="col-sm-1 chess-square" style={this.styles.square}>
-                        <Square isLight={true} /><Square isLight={false} /><Square isLight={true} /><Square isLight={false} />
-                        <Square isLight={true} /><Square isLight={false} /><Square isLight={true} /><Square isLight={false} />
-                    </div>
-                    <div class="col-sm-1" style={this.styles.square}>
-                        <Square isLight={false} /><Square isLight={true} /><Square isLight={false} /><Square isLight={true} />
-                        <Square isLight={false} /><Square isLight={true} /><Square isLight={false} /><Square isLight={true} />
-                    </div>
-                    <div class="col-sm-1" style={this.styles.square}>
-                        <Square isLight={true} /><Square isLight={false} /><Square isLight={true} /><Square isLight={false} />
-                        <Square isLight={true} /><Square isLight={false} /><Square isLight={true} /><Square isLight={false} />
-                    </div>
-                    <div class="col-sm-1" style={this.styles.square}>
-                        <Square isLight={false} /><Square isLight={true} /><Square isLight={false} /><Square isLight={true} />
-                        <Square isLight={false} /><Square isLight={true} /><Square isLight={false} /><Square isLight={true} />                  
-                    </div>
-                    <div class="col-sm-1" style={this.styles.square}>
-                        <Square isLight={true} /><Square isLight={false} /><Square isLight={true} /><Square isLight={false} />
-                        <Square isLight={true} /><Square isLight={false} /><Square isLight={true} /><Square isLight={false} />
-                    </div>
-                    <div class="col-sm-1" style={this.styles.square}>
-                        <Square isLight={false} /><Square isLight={true} /><Square isLight={false} /><Square isLight={true} />
-                        <Square isLight={false} /><Square isLight={true} /><Square isLight={false} /><Square isLight={true} />
-                    </div>
-                    <div class="col-sm-1" style={this.styles.square}>
-                        <Square isLight={true} /><Square isLight={false} /><Square isLight={true} /><Square isLight={false} />
-                        <Square isLight={true} /><Square isLight={false} /><Square isLight={true} /><Square isLight={false} />
-                    </div>
-                    <div class="col-sm-1" style={this.styles.square}>
-                        <Square isLight={false} /><Square isLight={true} /><Square isLight={false} /><Square isLight={true} />
-                        <Square isLight={false} /><Square isLight={true} /><Square isLight={false} /><Square isLight={true} />
-                    </div>
-                </div> 
-            </div>
-         );
-    }
+  state = {};
+  styles = {
+    board: {
+      width: "512px",
+      height: "512px",
+    },
+    square: {
+      width: "64px",
+      height: "64px",
+    },
+  };
+  render() {
+    return (
+      <div class="container ml-4">
+        <div class="row" style={this.styles.board}>
+          <div class="col-sm-1" style={this.styles.square}>
+            <Square isLight={true} />
+            <Square isLight={false} />
+            <Square isLight={true} />
+            <Square isLight={false} />
+            <Square isLight={true} />
+            <Square isLight={false} />
+            <Square isLight={true} />
+            <Square isLight={false} />
+          </div>
+          <div class="col-sm-1" style={this.styles.square}>
+            <Square isLight={false} />
+            <Square isLight={true} />
+            <Square isLight={false} />
+            <Square isLight={true} />
+            <Square isLight={false} />
+            <Square isLight={true} />
+            <Square isLight={false} />
+            <Square isLight={true} />
+          </div>
+          <div class="col-sm-1" style={this.styles.square}>
+            <Square isLight={true} />
+            <Square isLight={false} />
+            <Square isLight={true} />
+            <Square isLight={false} />
+            <Square isLight={true} />
+            <Square isLight={false} />
+            <Square isLight={true} />
+            <Square isLight={false} />
+          </div>
+          <div class="col-sm-1" style={this.styles.square}>
+            <Square isLight={false} />
+            <Square isLight={true} />
+            <Square isLight={false} />
+            <Square isLight={true} />
+            <Square isLight={false} />
+            <Square isLight={true} />
+            <Square isLight={false} />
+            <Square isLight={true} />
+          </div>
+          <div class="col-sm-1" style={this.styles.square}>
+            <Square isLight={true} />
+            <Square isLight={false} />
+            <Square isLight={true} />
+            <Square isLight={false} />
+            <Square isLight={true} />
+            <Square isLight={false} />
+            <Square isLight={true} />
+            <Square isLight={false} />
+          </div>
+          <div class="col-sm-1" style={this.styles.square}>
+            <Square isLight={false} />
+            <Square isLight={true} />
+            <Square isLight={false} />
+            <Square isLight={true} />
+            <Square isLight={false} />
+            <Square isLight={true} />
+            <Square isLight={false} />
+            <Square isLight={true} />
+          </div>
+          <div class="col-sm-1" style={this.styles.square}>
+            <Square isLight={true} />
+            <Square isLight={false} />
+            <Square isLight={true} />
+            <Square isLight={false} />
+            <Square isLight={true} />
+            <Square isLight={false} />
+            <Square isLight={true} />
+            <Square isLight={false} />
+          </div>
+          <div class="col-sm-1" style={this.styles.square}>
+            <Square isLight={false} />
+            <Square isLight={true} />
+            <Square isLight={false} />
+            <Square isLight={true} />
+            <Square isLight={false} />
+            <Square isLight={true} />
+            <Square isLight={false} />
+            <Square isLight={true} />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
- 
+
 export default Chessboard;
