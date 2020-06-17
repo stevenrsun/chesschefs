4513b67b19e65b2c011994025a1754f1d47eff13
randyyu13 <ldyu999@gmail.com>
2020-06-17 00:46:52 -0500

    Update counter.jsx

--- src/components/Game/counter.jsx
+++ src/components/Game/counter.jsx
@@ -1,40 +1,69 @@
 import React, { Component } from "react";
 import firebase from "firebase";
 import Chessboard from "./chessboard";
-import { AuthUserContext } from '../Session';
+import { AuthUserContext } from "../Session";
 
-const Counter = ({authUser}) => (
+const Counter = ({ authUser }) => (
   <div>
     <AuthUserContext.Consumer>
-            {authUser =>
-                authUser ? <CounterWithUID uid={authUser.uid}/> : <CounterWithUID uid={0}/>
-            }
+      {(authUser) =>
+        authUser ? (
+          <CounterWithUID uid={authUser.uid} />
+        ) : (
+          <CounterWithUID uid={0} />
+        )
+      }
     </AuthUserContext.Consumer>
   </div>
-)
+);
 
 class CounterWithUID extends Component {
   constructor() {
     super();
 
     this.database = firebase.database();
-    this.counter = this.database.ref().child("TEST_COUNTER");
+    this.counterOne = this.database.ref().child("TEST_COUNTER_ONE");
+    this.counterTwo = this.database.ref().child("TEST_COUNTER_TWO");
 
     this.state = {
-      count: 0
+      countOne: 0,
+      countTwo: 0,
+
+      firstUID: "",
+      secondUID: "",
     };
   }
 
   componentDidMount() {
-    this.counter.on("value", (snap) => {
+    this.counterOne.on("value", (snap) => {
       this.setState({
-        count: snap.val(),
+        countOne: snap.val(),
       });
     });
+
+    this.counterTwo.on("value", (snap) => {
+      this.setState({
+        countTwo: snap.val(),
+      });
+    });
+
+    if (this.state.firstUID === "") {
+      this.state.firstUID = this.props.uid;
+    } else {
+      this.state.secondUID = this.props.uid;
+    }
   }
 
-  incrementCounter = () => {
-    this.counter.set(this.state.count + 1);
+  incrementCounterOne = () => {
+    if (this.props.uid === this.state.firstUID) {
+      this.counterOne.set(this.state.countOne + 1);
+    }
+  };
+
+  incrementCounterTwo = () => {
+    if (this.props.uid === this.state.secondUID) {
+      this.counterTwo.set(this.state.countTwo + 1);
+    }
   };
 
   render() {
@@ -42,18 +71,36 @@ class CounterWithUID extends Component {
       <React.Fragment>
         <h1>{this.props.uid}</h1>
         <h1 style={{ marginTop: 50 }}>
-          This counter has been incremented: {this.state.count} times
+          firstUID: {this.state.firstUID}
+          <br />
+          your UID: {this.props.uid}
+          <br />
+          counterOne has been incremented: {this.state.countOne} times
+        </h1>
+        <h1 style={{ marginTop: 50 }}>
+          secondUID: {this.state.secondUID}
+          <br />
+          your UID: {this.props.uid}
+          <br />
+          counterTwo has been incremented: {this.state.countTwo} times
         </h1>
         <button
           type="button"
-          onClick={this.incrementCounter}
+          onClick={this.incrementCounterOne}
           class="btn btn-primary"
         >
-          Increment
+          Increment Button One
         </button>
 
-        <Chessboard/>
+        <button
+          type="button"
+          onClick={this.incrementCounterTwo}
+          class="btn btn-primary"
+        >
+          Increment Button Two
+        </button>
 
+        <Chessboard />
       </React.Fragment>
     );
   }
